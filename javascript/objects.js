aecb64144ac38133fc654127781c113e7bdb2633
Pat Lillis <lillispm@gmail.com>
2016-01-21 22:58:04 -0500

    remove all the node/grunt/babel stuff for now

--- javascript/objects.js
+++ javascript/objects.js
@@ -1,18 +1,14 @@
 
-class Point3D {
-    constructor (x, y, z) {
-        this.x = x || 0;
-        this.y = y || 0;
-        this.z = z || 0;
-    }
+function Point3D (x, y, z) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.z = z || 0;
 }
 
 
-class Point {
-    constructor (x, y) {
-        this.x = x || 0;
-        this.y = y || 0;
-    }
+function Point (x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
 }
 
 
